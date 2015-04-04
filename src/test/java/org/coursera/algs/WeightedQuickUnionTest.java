adf73261a9da9139786aee97b3e755e93a50da1f
Benny Zlotnik <uber442@gmail.com>
2015-04-04 18:15:10 +0300

    Add tests and flatten weighted tree after each root search

--- src/test/java/org/coursera/algs/WeightedQuickUnionTest.java
+++ src/test/java/org/coursera/algs/WeightedQuickUnionTest.java
@@ -13,14 +13,32 @@ public class WeightedQuickUnionTest {
 	}
 	
 	@Test
-	public void test_connectTrue() {
+	public void test_connectedDirectTrue() {
 		q.union(1, 2);
+
 		Assert.assertTrue(q.connected(1, 2));
 	}
 	
 	@Test
-	public void test_connectFalse() {
+	public void test_connectedIndirectTrue() {
+		q.union(1, 2);
+		q.union(2, 6);
+		q.union(6, 4560);
+		Assert.assertTrue(q.connected(4560, 1));
+	}
+	
+	@Test
+	public void test_connectedDirectFalse() {
 		Assert.assertFalse(q.connected(1, 2));
+	}
+	
+	@Test
+	public void test_connectedIndirectFalse() {
+		q.union(1, 2);
+		q.union(2, 6);
+		q.union(6, 4560);
+		q.union(7, 250);
+		Assert.assertFalse(q.connected(7, 2));
 	}
 
 }
